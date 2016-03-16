fed51a0c07ad416dba7c0cf74ecc99c77e3c5b08
bastianvandenbout <bastianbout@users.sourceforge.net>
2016-03-16 12:37:46 +0100

    BB - Kinematic wave 2D outflow boundary condition updated (boundary cells can not be pits!)

--- lisKinematic2d.cpp
+++ lisKinematic2d.cpp
@@ -235,7 +235,7 @@ double TWorld::K2DFlux()
         }
         if(K2DOutlets->Drc == 1)
         {
-            K2DQ->Drc =std::min(1.0,KinematicBoundaryFraction*dtr) *  (DX->Drc*K2DHOld->Drc*ChannelAdj->Drc);
+            K2DQ->Drc =std::min(0.5,KinematicBoundaryFraction*dtr) *  (DX->Drc*K2DHOld->Drc*ChannelAdj->Drc);
         }
 
     }
