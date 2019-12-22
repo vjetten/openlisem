f0a3b5a82848518c4ed420072073bce68f61f741
Victor Jetten <v.g.jetten@utwente.nl>
2019-12-22 02:59:26 +0100

    //

--- include/version.h
+++ include/version.h
@@ -35,8 +35,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONNR "5.9beta"
-#define DATE "2019/12/04"
+#define VERSIONNR "5.9"
+#define DATE "2019/12/22"
 
 #define VERSION QString("openLISEM version %1 - %2").arg(VERSIONNR).arg(DATE)
 
