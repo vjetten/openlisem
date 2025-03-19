77902370b54019ab6cee0bd99f917881936ed5b9
Victor Jetten <v.jetten@kpnmail.nl>
2025-03-19 10:43:46 +0100

    //

--- flow/lisSWOF2DopenMUSCL.cpp
+++ flow/lisSWOF2DopenMUSCL.cpp
@@ -136,20 +136,20 @@ double TWorld::fullSWOF2openMUSCL(cTMap *h, cTMap *u, cTMap *v, cTMap *z)
             } else
                 if (c+1 <= _nrCols-1 && MV(r,c+1) && !MV(r,c-1)) {
                     if (u->Drc > 0)
-                        QB1 = u->Drc*h->Drc*ChannelAdj->Drc;
+                        QB2 = u->Drc*h->Drc*ChannelAdj->Drc;
                 } else
                     if (r-1 >= 0 && MV(r-1,c) && !MV(r+1,c)) {
                         if (v->Drc < 0)
-                            QB1 = v->Drc*h->Drc*ChannelAdj->Drc;
+                            QB3 = v->Drc*h->Drc*ChannelAdj->Drc;
                     } else
                         if (r+1 <= _nrRows-1 && MV(r+1,c) && !MV(r-1,c)) {
                             if (v->Drc > 0)
-                                QB1 = v->Drc*h->Drc*ChannelAdj->Drc;
+                                QB4 = v->Drc*h->Drc*ChannelAdj->Drc;
                         }
-            QBF += fabs(QB1);//+QB2+QB3+QB4;
+            QBF += fabs(QB1+QB2+QB3+QB4);
         }
     }}
-sumh += QBF*_dt;
+//sumh += QBF*_dt;
     correctMassBalance(sumh, h, 0);
 
     QBoundary = QBF;
@@ -250,12 +250,12 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
                 u_y2 = U;
                 v_y2 = V;
             }
-
+/*
             // boundary cell fluxes
             // if there are barriers at the flowdomain edge, these should be done here!
             // calculate inner cell boundary and use that for outer cell value
             if (FlowBoundary->Drc > 0) {
-                double factor = 0.05;
+                double factor = 0.01;
                 //if left does not exist and right exist estimate gradient
                 if (c-1 >= 0 && MV(r,c-1) && !MV(r,c+1)) {
                    // z_x1 = 2*Z-z_x2;
@@ -271,6 +271,7 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
 
                        u_x1 = LIMIT(u_x1,U);
                        v_x1 = LIMIT(v_x1,V);
+
                     } else {
                         u_x1 = 0;
                         v_x1 = 0;
@@ -296,6 +297,7 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
 
                         u_x2 = LIMIT(u_x2,U);
                         v_x2 = LIMIT(v_x2,V);
+
                     }else {
                         u_x2 = 0;
                         v_x2 = 0;
@@ -315,6 +317,7 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
 
                        u_y1 = LIMIT(u_y1,U);
                        v_y1 = LIMIT(v_y1,V);
+
                     }else {
                         u_y1 = 0;
                         v_y1 = 0;
@@ -340,13 +343,15 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
 
                        u_y2 = LIMIT(u_y2,U);
                        v_y2 = LIMIT(v_y2,V);
+                       u_y2 = U;
+                       v_y2 = V;
                     }else {
                         u_y2 = 0;
                         v_y2 = 0;
                     }
                 }
             }
-
+*/
             dz_x1 = (Z - z_x1);
             dz_x2 = (z_x2 - Z);
             dz_y1 = (Z - z_y1);
@@ -539,7 +544,7 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
             //  2e component [1]: Momentum flux direction of flow ( m4/s2)/(m) = m3/s2 = h*u*u)
             //  3d component [3]: Momentum flux perpendicular to flow ( (m4/s2)/(m) = m3/s2 = h*u*v)
             //  4th component[3]: celerity (time)
-
+/*
             //left and right hand side of cells c and c-1 (x and x1)
             h_x1r = std::max(0.0, hx1r - std::max(0.0,  dz_x1 + fb_x1)); //rechts van c-1
             h_xl  = std::max(0.0, hxl  - std::max(0.0, -dz_x1 + fb_x1)); //links van het midden
@@ -557,6 +562,42 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
 
             h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));
             h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));
+            hll_y2 = F_Riemann(h_yd,vyd,uyd, h_y2u,vy2u,uy2u); // r and r+1
+*/
+
+            //left and right hand side of c and c-1 (x and x1)
+            if (bc1) {
+                h_x1r = std::max(0.0, hx1r - std::max(0.0,  dz_x1 + fb_x1)); //rechts van c-1
+                h_xl  = std::max(0.0, hxl  - std::max(0.0, -dz_x1 + fb_x1)); //links van het midden
+            } else {
+                h_x1r=ux1r=vx1r=0.0;
+            } // if !inside = boundary
+            hll_x1 = F_Riemann(h_x1r,ux1r,vx1r, h_xl,uxl,vxl); // c-1 (x1 right) and c (x1 left)
+
+            //right and left hand side of c and c+1 (x and x2)
+            if (bc2) {
+                h_xr  = std::max(0.0, hxr  - std::max(0.0,  dz_x2 + fb_x2));
+                h_x2l = std::max(0.0, hx2l - std::max(0.0, -dz_x2 + fb_x2));
+            } else {
+                h_x2l=ux2l=vx2l=0.0;
+            }
+            hll_x2 = F_Riemann(h_xr,uxr,vxr, h_x2l,ux2l,vx2l); // c and c+1
+
+            if (br1) {
+                h_y1d = std::max(0.0, hy1d - std::max(0.0,  dz_y1 + fb_y1));
+                h_yu  = std::max(0.0, hyu  - std::max(0.0, -dz_y1 + fb_y1));
+            } else {
+                h_y1d=vy1d=uy1d=0.0;
+            }
+            hll_y1 = F_Riemann(h_y1d,vy1d,uy1d, h_yu,vyu,uyu); // r-1 (y1 down) and r (y up)
+            // v and u chnaged places for y comnpared to x ? why? is also in swof code
+
+            if (br2) {
+                h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));
+                h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));
+            } else {
+                h_y2u=vy2u=uy2u=0.0;
+            }
             hll_y2 = F_Riemann(h_yd,vyd,uyd, h_y2u,vy2u,uy2u); // r and r+1
 
             // determine smallest dt in x and y for each cell
