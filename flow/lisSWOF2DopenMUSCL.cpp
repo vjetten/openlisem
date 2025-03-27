f62e649717719f9d3965aaf04aec73fd1014c9b1
vjetten <v.jetten@kpnmail.nl>
2025-03-27 07:52:05 +0100

    // checked Riemann boundary conditions, this code is the best option

--- flow/lisSWOF2DopenMUSCL.cpp
+++ flow/lisSWOF2DopenMUSCL.cpp
@@ -68,7 +68,7 @@ double TWorld::fullSWOF2openMUSCL(cTMap *h, cTMap *u, cTMap *v, cTMap *z)
         // in the original code this is split in reconstruction/MUSCL and maincalcflux
 
         doSWOFStV(dt_req_min, h, u, v);
-        // Saint Venand calculations for new h, u, v
+        // Saint-Venant calculations for new h, u, v
         // called maincalcscheme in fullSWOF
 
         //until here is first order ! just one calculation
@@ -478,7 +478,14 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
                 h_x1r = std::max(0.0, hx1r - std::max(0.0,  dz_x1 + fb_x1)); //rechts van c-1
                 h_xl  = std::max(0.0, hxl  - std::max(0.0, -dz_x1 + fb_x1)); //links van het midden
             } else {
-          //      h_x1r=ux1r=vx1r=0.0;
+               // h_x1r=ux1r=vx1r=0.0;
+                // FOR REFERNCE, ALL ATTEMPTS GIVE NAN,
+                // h_x1r=H;
+                // ux1r=U;
+                // vx1r=V;
+                // h_x1r=h_x1;
+                // ux1r=u_x1;
+                // vx1r=v_x1;
             }
             hll_x1 = F_Riemann(h_x1r,ux1r,vx1r, h_xl,uxl,vxl); // c-1 (x1 right) and c (x1 left)
 
@@ -487,7 +494,13 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
                 h_xr  = std::max(0.0, hxr  - std::max(0.0,  dz_x2 + fb_x2));
                 h_x2l = std::max(0.0, hx2l - std::max(0.0, -dz_x2 + fb_x2));
             } else {
-           //     h_x2l=ux2l=vx2l=0.0;
+              //  h_x2l=ux2l=vx2l=0.0;
+                // h_x2l=H;
+                // ux2l=U;
+                // vx2l=V;
+                // h_x2l=h_x2;
+                // ux2l=u_x2;
+                // vx2l=v_x2;
             }
             hll_x2 = F_Riemann(h_xr,uxr,vxr, h_x2l,ux2l,vx2l); // c and c+1
 
@@ -495,16 +508,30 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
                 h_y1d = std::max(0.0, hy1d - std::max(0.0,  dz_y1 + fb_y1));
                 h_yu  = std::max(0.0, hyu  - std::max(0.0, -dz_y1 + fb_y1));
             } else {
-           //     h_y1d=vy1d=uy1d=0.0;
+              //  h_y1d=vy1d=uy1d=0.0;
+                // h_y1d=H;
+                // uy1d=U;
+                // vy1d=V;
+                // h_y1d=h_y1;
+                // uy1d=u_y1;
+                // vy1d=v_y1;
             }
             hll_y1 = F_Riemann(h_y1d,vy1d,uy1d, h_yu,vyu,uyu); // r-1 (y1 down) and r (y up)
             // v and u chnaged places for y comnpared to x ? why? is also in swof code
 
             if (br2) {
-                h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));
-                h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));
+                h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));// lower side of upper cell
+                h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));// upper side of lower cell
             } else {
-          //      h_y2u=vy2u=uy2u=0.0;
+              //  h_y2u=vy2u=uy2u=0.0;
+                //h_y2u = std::max(0.0, h_y2 - std::max(0.0, -dz_y2 + fb_y2));
+                // h_y2u=H;
+                // uy2u=U;
+                // vy2u=V;
+                // h_y2u=h_y2;
+                // uy2u=u_y2;
+                // vy2u=v_y2;
+                qDebug() << h_y2u << vy2u <<uy2u;
             }
             hll_y2 = F_Riemann(h_yd,vyd,uyd, h_y2u,vy2u,uy2u); // r and r+1
 
@@ -513,7 +540,7 @@ double TWorld::doSWOFMUSCLdt(double dt, double timesum, cTMap *h, cTMap *u, cTMa
             double dty = courant_factor*dy/std::max(hll_y1.v[3],hll_y2.v[3]);
             FloodDT->Drc = std::min(dtx, dty);
 
-            // save the Riemann results in maps, needed for Saint Venand
+            // save the Riemann results in maps, needed for Saint-Venant
             gflowx->Drc = GRAV*0.5*( (h_xl-hxl)*(h_xl+hxl) + (hxr-h_xr)*(hxr+h_xr) + delzcx*(hxl+hxr) ); // delzcx = 0 if not muscl
             gflowy->Drc = GRAV*0.5*( (h_yu-hyu)*(h_yu+hyu) + (hyd-h_yd)*(hyd+h_yd) + delzcy*(hyu+hyd) );
             hllx12_0->Drc = hll_x1.v[0] - hll_x2.v[0];
@@ -599,64 +626,3 @@ void TWorld::doSWOFStV(double dt, cTMap *h, cTMap *u, cTMap *v)
 }
 //-----------------------------------------------------------------------------------------------------------
 
-
-
-
-
-/*
-        //left and right hand side of c and c-1 (x and x1)
-        if (bc1) {
-            h_x1r = std::max(0.0, hx1r - std::max(0.0,  dz_x1 + fb_x1)); //rechts van c-1
-            h_xl  = std::max(0.0, hxl  - std::max(0.0, -dz_x1 + fb_x1)); //links van het midden
-        } else {
-            h_x1r=ux1r=vx1r=0.0;
-        } // if !inside = boundary
-        hll_x1 = F_Riemann(h_x1r,ux1r,vx1r, h_xl,uxl,vxl); // c-1 (x1 right) and c (x1 left)
-
-        //right and left hand side of c and c+1 (x and x2)
-        if (bc2) {
-            h_xr  = std::max(0.0, hxr  - std::max(0.0,  dz_x2 + fb_x2));
-            h_x2l = std::max(0.0, hx2l - std::max(0.0, -dz_x2 + fb_x2));
-        } else {
-            h_x2l=ux2l=vx2l=0.0;
-        }
-        hll_x2 = F_Riemann(h_xr,uxr,vxr, h_x2l,ux2l,vx2l); // c and c+1
-
-        if (br1) {
-            h_y1d = std::max(0.0, hy1d - std::max(0.0,  dz_y1 + fb_y1));
-            h_yu  = std::max(0.0, hyu  - std::max(0.0, -dz_y1 + fb_y1));
-        } else {
-            h_y1d=vy1d=uy1d=0.0;
-        }
-        hll_y1 = F_Riemann(h_y1d,vy1d,uy1d, h_yu,vyu,uyu); // r-1 (y1 down) and r (y up)
-        // v and u chnaged places for y comnpared to x ? why? is also in swof code
-
-        if (br2) {
-            h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));
-            h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));
-        } else {
-            h_y2u=vy2u=uy2u=0.0;
-        }
-        hll_y2 = F_Riemann(h_yd,vyd,uyd, h_y2u,vy2u,uy2u); // r and r+1
-*/
-
-/*
-            //left and right hand side of cells c and c-1 (x and x1)
-            h_x1r = std::max(0.0, hx1r - std::max(0.0,  dz_x1 + fb_x1)); //rechts van c-1
-            h_xl  = std::max(0.0, hxl  - std::max(0.0, -dz_x1 + fb_x1)); //links van het midden
-            hll_x1 = F_Riemann(h_x1r,ux1r,vx1r, h_xl,uxl,vxl); // c-1 (x1 right) and c (x1 left)
-
-            //right and left hand side of c and c+1 (x and x2)
-            h_xr  = std::max(0.0, hxr  - std::max(0.0,  dz_x2 + fb_x2));
-            h_x2l = std::max(0.0, hx2l - std::max(0.0, -dz_x2 + fb_x2));
-            hll_x2 = F_Riemann(h_xr,uxr,vxr, h_x2l,ux2l,vx2l); // c and c+1
-
-            // v and u chnaged places for y, so that parallel and perpendicuar to flow remain the same for x and y
-            h_y1d = std::max(0.0, hy1d - std::max(0.0,  dz_y1 + fb_y1));
-            h_yu  = std::max(0.0, hyu  - std::max(0.0, -dz_y1 + fb_y1));
-            hll_y1 = F_Riemann(h_y1d,vy1d,uy1d, h_yu,vyu,uyu); // r-1 (y1 down) and r (y up)
-
-            h_yd  = std::max(0.0, hyd  - std::max(0.0,  dz_y2 + fb_y2));
-            h_y2u = std::max(0.0, hy2u - std::max(0.0, -dz_y2 + fb_y2));
-            hll_y2 = F_Riemann(h_yd,vyd,uyd, h_y2u,vy2u,uy2u); // r and r+1
-*/
