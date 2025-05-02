9ee4950d61cd1e4e48f5dbd2c7554b71bedc6263
vjetten <v.jetten@kpnmail.nl>
2025-05-02 08:40:28 +0200

    //

--- channel/lisChannelflood.cpp
+++ channel/lisChannelflood.cpp
@@ -162,23 +162,6 @@ void TWorld::ChannelOverflow(cTMap *_h, cTMap *V)
              }
          }
      }}
-
-    #pragma omp parallel for num_threads(userCores)
-    FOR_ROW_COL_MV_CHL {
-        if (ChannelMaxQ->Drc <= 0) {
-            ChannelWaterVol->Drc = ChannelWH->Drc * ChannelDX->Drc * ChannelWidth->Drc;
-            WaterVolall->Drc = CHAdjDX->Drc*_h->Drc + MicroStoreVol->Drc;
-
-            if(SwitchErosion) {
-                SWOFSedimentLayerDepth(r,c,_h->Drc, V->Drc);
-                SWOFSedimentSetConcentration(r,c, _h->Drc, ChannelAdj->Drc);
-
-                RiverSedimentLayerDepth(r, c);
-                RiverSedimentMaxC(r, c);
-                // all concentrations, possible ChannelDep when surplus
-            }
-        }
-    }}
 }
 
 //---------------------------------------------------------------------------
@@ -385,6 +368,28 @@ void TWorld::ChannelFlood(void)
         ChannelOverflow(hmxrunoff, V);
     // determine overflow water => hmx
     // hmx is flood water, WH is overlandflow, WHrunoff etc
+
+
+    #pragma omp parallel for num_threads(userCores)
+    FOR_ROW_COL_MV_CHL {
+        if (ChannelMaxQ->Drc <= 0 && hmxrunoff->Drc > 0) {
+            ChannelWaterVol->Drc = ChannelWH->Drc * ChannelDX->Drc * ChannelWidth->Drc;
+            hmx->Drc = hmxrunoff->Drc + WHstore->Drc;
+            hmxWH->Drc = hmx->Drc + WH->Drc;
+            WaterVolall->Drc = CHAdjDX->Drc*hmxWH->Drc;
+
+            if(SwitchErosion) {
+                // or hmx?
+                SWOFSedimentLayerDepth(r,c, hmxrunoff->Drc, V->Drc);
+                SWOFSedimentSetConcentration(r,c, hmxrunoff->Drc, ChannelAdj->Drc);
+
+                RiverSedimentLayerDepth(r, c);
+                RiverSedimentMaxC(r, c);
+                // all concentrations, possible ChannelDep when surplus
+            }
+        }
+    }}
+
 
     startFlood = false;
     #pragma omp parallel for num_threads(userCores)
--- flow/lisOverlandflow.cpp
+++ flow/lisOverlandflow.cpp
@@ -85,6 +85,26 @@ void TWorld::OverlandFlow2Ddyn(void)
     // // Mixing of 2D runoff with channel water, V is used to determine how much flows into the channel
     // // after this new ChannelHW and WHrunoff, and Susp sediment values ChannelSSSed and SSFlood->Drc
 
+    #pragma omp parallel for num_threads(userCores)
+    FOR_ROW_COL_MV_CHL {
+        if (ChannelMaxQ->Drc <= 0) {
+            ChannelWaterVol->Drc = ChannelWH->Drc * ChannelDX->Drc * ChannelWidth->Drc;
+            WH->Drc = WHrunoff->Drc + WHstore->Drc;
+            hmxWH->Drc = hmx->Drc + WH->Drc;
+            WaterVolall->Drc = CHAdjDX->Drc*hmxWH->Drc;
+
+            if(SwitchErosion) {
+                // or WH?
+                SWOFSedimentLayerDepth(r,c, WHrunoff->Drc, V->Drc);
+                SWOFSedimentSetConcentration(r,c, WHrunoff->Drc, ChannelAdj->Drc);
+
+                RiverSedimentLayerDepth(r, c);
+                RiverSedimentMaxC(r, c);
+                // all concentrations, possible ChannelDep when surplus
+            }
+        }
+    }}
+
     startFlood = false;
     #pragma omp parallel for num_threads(userCores)
     FOR_ROW_COL_MV_L {
