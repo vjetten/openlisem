810f8022cbcaa95ce88dd712dd1d205be8293f6d
vjetten <v.jetten@kpnmail.nl>
2025-05-14 07:32:01 +0200

    7.4.6.beta.R2

--- include/version.h
+++ include/version.h
@@ -37,7 +37,7 @@
 #define VERSION_H_
 
 
-#define VERSIONNR "7.4.6.beta.R"
+#define VERSIONNR "7.4.6.beta.R2"
 #define VERSIONDATE "2025/05/13"
 
 #define VERSION QString("openLISEM version %1 - %2").arg(VERSIONNR).arg(VERSIONDATE)
