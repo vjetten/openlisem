a8eea98cc740fd264e98c2430d6e824e499e2c6f
vjetten <v.jetten@kpnmail.nl>
2025-09-11 12:45:27 +0200

    //

--- flow/lisSWOF2Daux.cpp
+++ flow/lisSWOF2Daux.cpp
@@ -604,6 +604,4 @@ void TWorld::floodCount(cTMap *h)
         if (h->Drc == 0 and tmb->Drc == 0)
             floodFill(h,tmb, r,c,currentlabel++);
     }}
-// report(*h,"h");
-// report(*tmb,"ff");
 }
